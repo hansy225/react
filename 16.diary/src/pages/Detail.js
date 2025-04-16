b47b38568819c0966fdaeba68f97f4e39628d884
HanSuyeon <sy02251105@gmail.com>
2025-04-16 10:09:41 +0900

    0416

--- 16.diary/src/pages/Detail.js
+++ 16.diary/src/pages/Detail.js
@@ -3,23 +3,27 @@ import Button from "../components/Button";
 import Editor from "./Editor";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
+import { getEmotionImg } from './../util/emotion-img';
 
 const Detail = () => {
     const nav = useNavigate();
-    // const { id } = useParams();
-    // const [data, setData] = useEffect(null);
+    const { id } = useParams();
+    const [data, setData] = useEffect();
 
-    // useEffect(() => {
-
-    // })
 
     return (
         <div>
             <Header 
                 title={"일기 상세보기"}
                 leftChild={<Button text={"이전"} onClick={() => {nav(-1)}}/>}
+                rightChild = {<Button text={"수정하기"} onClick={() => nav('/edit/emotionItem.id')} />}
             />
-            <Editor />
+            <section className="img_section">
+                <h4>오늘의 감정</h4>
+                <div>
+                    <img src={getEmotionImg(EmotionItem.id)} />
+                </div>
+            </section>
         </div>
     )
 }
